d4859c471d26045b296c952504e99ce7dd09183f
Melanie Beck <melaniebeck81@gmail.com>
2021-08-19 13:04:14 -0500

    general refactor for AMP

--- rebrief/models.py
+++ rebrief/models.py
@@ -1,3 +1,43 @@
+# ###########################################################################
+#
+#  CLOUDERA APPLIED MACHINE LEARNING PROTOTYPE (AMP)
+#  (C) Cloudera, Inc. 2021
+#  All rights reserved.
+#
+#  Applicable Open Source License: Apache 2.0
+#
+#  NOTE: Cloudera open source products are modular software products
+#  made up of hundreds of individual components, each of which was
+#  individually copyrighted.  Each Cloudera open source product is a
+#  collective work under U.S. Copyright Law. Your license to use the
+#  collective work is as provided in your written agreement with
+#  Cloudera.  Used apart from the collective work, this file is
+#  licensed for your use pursuant to the open source license
+#  identified above.
+#
+#  This code is provided to you pursuant a written agreement with
+#  (i) Cloudera, Inc. or (ii) a third-party authorized to distribute
+#  this code. If you do not have a written agreement with Cloudera nor
+#  with an authorized and properly licensed third party, you do not
+#  have any rights to access nor to use this code.
+#
+#  Absent a written agreement with Cloudera, Inc. (“Cloudera”) to the
+#  contrary, A) CLOUDERA PROVIDES THIS CODE TO YOU WITHOUT WARRANTIES OF ANY
+#  KIND; (B) CLOUDERA DISCLAIMS ANY AND ALL EXPRESS AND IMPLIED
+#  WARRANTIES WITH RESPECT TO THIS CODE, INCLUDING BUT NOT LIMITED TO
+#  IMPLIED WARRANTIES OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY AND
+#  FITNESS FOR A PARTICULAR PURPOSE; (C) CLOUDERA IS NOT LIABLE TO YOU,
+#  AND WILL NOT DEFEND, INDEMNIFY, NOR HOLD YOU HARMLESS FOR ANY CLAIMS
+#  ARISING FROM OR RELATED TO THE CODE; AND (D)WITH RESPECT TO YOUR EXERCISE
+#  OF ANY RIGHTS GRANTED TO YOU FOR THE CODE, CLOUDERA IS NOT LIABLE FOR ANY
+#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR
+#  CONSEQUENTIAL DAMAGES INCLUDING, BUT NOT LIMITED TO, DAMAGES
+#  RELATED TO LOST REVENUE, LOST PROFITS, LOSS OF INCOME, LOSS OF
+#  BUSINESS ADVANTAGE OR UNAVAILABILITY, OR LOSS OR CORRUPTION OF
+#  DATA.
+#
+# ###########################################################################
+
 import typing
 
 import networkx as nx
@@ -6,6 +46,7 @@ import spacy
 from spacy.tokens import Doc 
 import pytextrank
 import torch
+import numpy as np
 
 # better sentencebert model
 SBERT_MODEL_NAME = 'sentence-transformers/paraphrase-mpnet-base-v2'
@@ -19,9 +60,13 @@ SBERT_CONFIG = {
     }
 }
 
-def build_nlp_pipeline(config=SBERT_CONFIG):
+def build_classic_nlp_pipeline():
     nlp = spacy.load("en_core_web_sm")
     nlp.add_pipe("textrank", config={ "token_lookback": 10 })
+    return nlp
+
+def build_trf_nlp_pipeline(config=SBERT_CONFIG):
+    nlp = spacy.load("en_core_web_sm")
     nlp.add_pipe("sentencizer")
     transformer = nlp.add_pipe("transformer", config=SBERT_CONFIG)
     # initialize the transformer model
@@ -33,7 +78,7 @@ def build_nlp_pipeline(config=SBERT_CONFIG):
 
 
 class SentenceTextRank:
-    def __init__(self, doc: Doc): 
+    def __init__(self, doc): 
         self.doc = doc
         self.transformer_ranks = self.trfembeddings_textrank()
         self.wordembedding_ranks = self.wordembeddings_textrank()
@@ -56,7 +101,7 @@ class SentenceTextRank:
 
     def trfembeddings_textrank(self):
         sentence_vectors = self.get_transformer_embeddings()
-        return self._sentence_rank(sentence_vectors)  
+        return self._sentence_rank(np.array(sentence_vectors.cpu()))  
 
     def wordembeddings_textrank(self):
         sentence_vectors = self.get_sentence_embeddings()
@@ -69,7 +114,11 @@ class SentenceTextRank:
         return self._mean_pooling(token_embeddings, attn_mask)
 
     def get_sentence_embeddings(self):
-        return [sent.vector for sent in self.doc.sents] 
+        try:
+          # if using spaCy on GPU, convert cupy arr --> numpy arr with get()
+          return [sent.vector.get() for sent in self.doc.sents] 
+        except:
+          return [sent.vector for sent in self.doc.sents] 
 
     def _mean_pooling(self, token_embeddings, attention_mask):
         """ Take attention mask into account for correct averaging of sentence-bert tokens """
