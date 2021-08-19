87df71540b028b2b2ecd3f3dbb433bae741071f8
Melanie Beck <melaniebeck81@gmail.com>
2021-08-19 13:03:41 -0500

    revamped rebrief to v2.0 in which we now have access to several models

--- apps/rebrief-app.py
+++ apps/rebrief-app.py
@@ -39,6 +39,7 @@
 # ###########################################################################
 
 import re
+from dataclasses import dataclass
 
 from selenium import webdriver  
 from selenium.webdriver.chrome.options import Options
@@ -46,25 +47,35 @@ import wikipedia as wiki
 import streamlit as st
 st.set_page_config(layout="wide")
 
-from transformers import pipeline
-
 from rebrief.utils import match_most_text, highlight_text
+from rebrief.st_model_wrappers import (
+    abstractive, 
+    modern_extractive, 
+    classic_extractive, 
+    upgraded_classic_extractive
+)
 
 CHROMEDRIVER_PATH = '/Users/mbeck/Projects/chromedriver'
 SCREENSHOT_WINDOW_SIZE = "1500,900" 
 
+MODELS = (
+    abstractive,
+    modern_extractive,
+    classic_extractive,
+    upgraded_classic_extractive
+)
 
+# Global versions for caching
 @st.cache(allow_output_mutation=True)
-def load_model():
-    model = pipeline("summarization")
-    return model
+def load_model(model):
+    return model.load()
+
+# Global versions for caching
+@st.cache(allow_output_mutation=True)
+def summarize_text(article, model):
+    return model.summarize(article, load_model(model))
 
 @st.cache()
-def summarize_text(article, model_name=None):
-    model = load_model()
-    summary = model(article)
-    return summary
-
 def make_screenshot(url, output_filename):
     """ Use a headless browser to take a screenshot of the provided url and save to file. """
     chrome_options = Options()  
@@ -81,23 +92,26 @@ def make_screenshot(url, output_filename):
     driver.save_screenshot(output_filename)
     driver.close()
 
+@st.cache()
 def parse_summary(summary):
     """ Split a Transformers Pipeline Summary object into snippets. """
     snippets = re.split('[,.]', summary[0]['summary_text'])
     snippets = [s.strip() for s in snippets if len(s) > 1]
     return snippets
 
+@st.cache()
 def make_url(text, url):
     """ Add HTML to convert text into a clickable url. """
     new_text = f'<a target="_blank" href="{url}">{text}</a>'
     return new_text
 
 
-# ------ SIDEBAR SELECTIONS ------
+# ============ SIDEBAR ============
 st.sidebar.title("Summarization on Wikipedia articles")
 st.sidebar.markdown("Currently using HuggingFace models for Abstractive Summarization. But as you'll see below, \
     the summaries are often verbatim from the original text (highlighted).")
 
+# ----- Article Selection -----
 articles = {
     "Machine learning": wiki.search("machine learning", results=1),
     "Birds": wiki.search("birds", results=1),
@@ -108,14 +122,21 @@ articles = {
 st.sidebar.markdown("### Select Things.")
 article_selection = st.sidebar.selectbox("Choose one of my hobbies to summarize", list(articles.keys()))
 
-model_selection = st.sidebar.selectbox("Choose a summarization model:", ['There', 'Are', 'No','Options'])
+# ----- Model Selection -----
+model_selector = {m.display_name: m for m in MODELS}
+model_obj = model_selector[
+    st.sidebar.selectbox("Choose a summarization model:", list(model_selector.keys()))
+    ]
 
-# ------ BEGIN APP ------
+# ----- Model Description -----
+st.sidebar.markdown(model_obj.description)
 
+# ============ MAIN PAGE ============
+# load article
 wiki_page = wiki.page(article_selection, auto_suggest=False)
 article = wiki_page.summary
 
-# Display Article Title and Image
+# ----- Display Article Title and Image -----
 title_url = make_url(article_selection, wiki_page.url)
 image_name = f"images/{article_selection.replace(' ', '_')}.png"
 screenshot = make_screenshot(wiki_page.url, image_name)
@@ -124,19 +145,19 @@ _, img_col, _ = st.beta_columns(3)
 img_col.markdown(f"### {title_url}", unsafe_allow_html=True)
 img_col.image(image_name)
 
-# Display Summary and original
+# ----- Display Summary and original -----
 col1, col2 = st.beta_columns(2)
 
 # extract answers
-summary = summarize_text(article)
+summary = summarize_text(article, model_obj)
 
 col1.subheader('Model Summary')
-col1.write(f"\n{summary[0]['summary_text']}")
+col1.write(f"\n{summary}")
 
 col2.subheader('Original Wikipedia article')
 col2.markdown("##### (actually, only the first section which is itself a summary)")
 
-snippets = match_most_text(summary[0]['summary_text'], article)
+snippets = match_most_text(summary, article)
 highlighted_article = highlight_text(snippets, article)
 for paragraph in highlighted_article.split("\n"):
     col2.write(paragraph, unsafe_allow_html=True)
