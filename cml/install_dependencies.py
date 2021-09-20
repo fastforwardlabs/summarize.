f3368ba285b83259811e247518bb577f6db8d47d
Melanie Beck <melaniebeck81@gmail.com>
2021-09-20 16:21:49 -0500

    test python3 kernel dependency

--- cml/install_dependencies.py
+++ cml/install_dependencies.py
@@ -38,5 +38,5 @@
 #
 # ###########################################################################
 
-!pip3 install -r requirements.txt
-!python3 -m spacy download en_core_web_sm
+!pip install -r requirements.txt
+!python -m spacy download en_core_web_sm
