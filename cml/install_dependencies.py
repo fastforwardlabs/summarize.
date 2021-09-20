5e2c6f7344c90811dfc76d0626b960a836b6c820
Melanie Beck <melaniebeck81@gmail.com>
2021-09-20 16:58:25 -0500

    put install stuff back the way it was with a python3 command for the spacy model download instaed of python

--- cml/install_dependencies.py
+++ cml/install_dependencies.py
@@ -38,4 +38,5 @@
 #
 # ###########################################################################
 
-!pip install -r requirements.txt
+!pip3 install -r requirements.txt
+!python3 -m spacy download en_core_web_sm
